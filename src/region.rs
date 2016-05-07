2d6bf25552aacb08de2f2875fbdba29fae964dcc
benaryorg <binary@benary.org>
2016-05-07 21:21:10 +0200

    Region/SuperRegion are parsable
    Signed-off-by: benaryorg <binary@benary.org>

--- src/region.rs
+++ src/region.rs
@@ -1,3 +1,8 @@
+use std::str::FromStr;
+
+#[derive(Debug,Clone,PartialEq)]
+pub struct ParseRegionError;
+
 #[derive(Debug,Clone,PartialEq,Eq,PartialOrd,Ord)]
 pub struct Region
 {
@@ -6,5 +11,23 @@ pub struct Region
 	super_region: usize,
 	player: Option<String>,
 	neighbours: Vec<usize>,
+}
+
+impl FromStr for Region
+{
+	type Err = ParseRegionError;
+
+	fn from_str(s: &str) -> Result<Self,Self::Err>
+	{
+		let mut sp = s.split_whitespace();
+		Ok(Region
+		{
+			id: sp.next().unwrap().parse().unwrap(),
+			count: 0,
+			super_region: sp.next().unwrap().parse().unwrap(),
+			player: None,
+			neighbours: Vec::new(),
+		})
+	}
 }
 
--- src/superregion.rs
+++ src/superregion.rs
@@ -1,7 +1,27 @@
+use std::str::FromStr;
+
+#[derive(Debug,Clone,PartialEq)]
+pub struct ParseSuperRegionError;
+
 #[derive(Debug,Clone,PartialEq,Eq,PartialOrd,Ord)]
 pub struct SuperRegion
 {
 	id: usize,
 	bonus: usize,
+}
+
+impl FromStr for SuperRegion
+{
+	type Err = ParseSuperRegionError;
+
+	fn from_str(s: &str) -> Result<Self,Self::Err>
+	{
+		let mut sp = s.split_whitespace();
+		Ok(SuperRegion
+		{
+			id: sp.next().unwrap().parse().unwrap(),
+			bonus: sp.next().unwrap().parse().unwrap(),
+		})
+	}
 }
 
